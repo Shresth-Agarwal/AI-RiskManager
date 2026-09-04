7358779da2d3d87b66dc91aa3a6e9bcf9561c891
Shresth-Agarwal <7shresthagarwal@gmail.com>
2026-09-04 14:08:00 +0530

    added cors and included batch endpoint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers.risk import router as risk_router
+from api.routers.batch import batch_router
 
 
 app = FastAPI(
@@ -9,4 +11,13 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(risk_router)
+app.include_router(batch_router)
